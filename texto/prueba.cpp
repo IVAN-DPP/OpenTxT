9479b369eec63e18045773e2cf24b1843bfd0528
Elchorco000 <ivandpp97@gmail.com>
2020-01-24 18:37:03 -0500

    Se crea un bucle para que en la salida de la función get_word_matrix tenga solo un tipo de palabra y no la misma palabra repetida varias veces

--- texto/prueba.cpp
+++ texto/prueba.cpp
@@ -66,8 +66,10 @@ vector<string> Prueba::get_paragraph(){
 }
 
 vector< vector<string> > Prueba::get_word_matrix(){
-  vector<string> list_of_words;
-  vector< vector<string> > matrix_of_words;
+  vector<string> word_vector;
+  vector< vector<string> > word_matrix;
+  vector<string> final_word_vector;
+  vector< vector<string> > final_word_matrix;
   string word;
   int temp=0;                                                                 //Indica el indice con el cual empieza una palabra 
   for(int j=0;j<get_paragraph().size();j++){                                  //Este primer bucle retorna la matriz de vectores fila
@@ -80,7 +82,7 @@ vector< vector<string> > Prueba::get_word_matrix(){
 	 get_paragraph()[j][i]=='?'){                                           //Si una palabra termina con punto, coma, sign interrogración, etc.
 	word=(get_paragraph()[j]).substr(temp,i-temp);                          //Guarda o asigna la palabra en word, empieza en temp y escoge (i-temp) elementos
 	temp=i+1;                                                               //Pone en temp el inicio de la siguiente palabra
-	list_of_words.push_back(word);                                          //Guarda la palabra en el vector "list_of_words"
+	word_vector.push_back(word);                                          //Guarda la palabra en el vector "word_vector"
       }
       else if(get_paragraph()[j][i-1]=='.' || get_paragraph()[j][i-1]==',' ||
 	      get_paragraph()[j][i-1]==')' || get_paragraph()[j][i-1]=='!' ||
@@ -100,7 +102,7 @@ vector< vector<string> > Prueba::get_word_matrix(){
       else if(get_paragraph()[j][i]=='"' && get_paragraph()[j][i+1]==' '){
 	word=(get_paragraph()[j]).substr(temp,i-temp);                          //Guarda o asigna la palabra en word, empieza en temp y escoge (i-temp) elementos
 	temp=i+1;                                                               //Pone en temp el inicio de la siguiente palabra
-	list_of_words.push_back(word);
+	word_vector.push_back(word);
       }
 
       else if(get_paragraph()[j][i-1]=='"' && get_paragraph()[j][i]==' ')
@@ -114,27 +116,45 @@ vector< vector<string> > Prueba::get_word_matrix(){
       else if(get_paragraph()[j][i]==' '){                                    //Se escoge una palabra que no termine con punto y coma
 	word=(get_paragraph()[j]).substr(temp,i-temp);                        
 	temp=i+1;                                                             
-	list_of_words.push_back(word);                                        
+	word_vector.push_back(word);                                        
       }
      
       else if(get_paragraph()[j][i]=='.' && get_paragraph()[j][i+1]=='\n'){  //Si se cumple esta condición es porque llego al final del parrafo
 	word=(get_paragraph()[j]).substr(temp,i-temp);
-	list_of_words.push_back(word);
+	word_vector.push_back(word);
 	break;
       }
       else if(get_paragraph()[j][i]=='\n')                                    //Por si el otro parrafo comienza con un salto de lineas
 	temp++;
     }
-    matrix_of_words.push_back(list_of_words);                                 //Guardamos el vector de palabras en el vector "matrix_of_words"
-    list_of_words.clear();                                                    //Limpiamos cualquier palabra del parrafo anterior
+    word_matrix.push_back(word_vector);                                       //Guardamos el vector de palabras en el vector "word_matrix"
+    word_vector.clear();                                                      //Limpiamos cualquier palabra del parrafo anterior
     temp=0;                                                                   //Inicializamos de cero de nuevo el indice con el que empieza la palabra
   }
 
   // La matriz se puede ver como si sus filas representaran los parrafos en el texto,
-  // p, Ej, matrix_of_word[1][] representa el segundo parrafo del texto, las columnas
+  // p, Ej, word_matrix[1][] representa el segundo parrafo del texto, las columnas
   // representan las palabras que tiene cada parrafo.
 
-  return matrix_of_words;
+  
+  for(int k=0;k<word_matrix.size();k++){                                     //Primer bucle para correr dendro de las filas de word_matrix
+    
+    final_word_vector.push_back(word_matrix[k][0]);                          //Guardamos la primera palabra para poder empezar la comparación
+    for(int j=0;j<(word_matrix[k]).size();j++){                              //Segundo bucle para correr dentro de las columnas de word_matrix
+      temp=0;                                                                //Esta variable realiza el conteo de las veces que se repite la palabra
+      for(int i=0;i<final_word_vector.size();i++)                            //Tercer bucle para correr dentro de nuestro nuevo vector y en el que guardaremos las palabras que no se repitan
+	if(final_word_vector[i]!=word_matrix[k][j]){                         //Este condicional busca si la palabra en word_matrix se repite en final_word_vector
+	  temp++;                                                            
+	  if(temp==final_word_vector.size())                                 //Si el condicional anterior se cumple para todas las palabras entonces la variable temp debera ser igual al tamaño del final_word_vector, ya que si no fuera igual entonces no la contaria y el tamaño de temp seria menor
+	    final_word_vector.push_back(word_matrix[k][j]);                  //Guardamos la palabra que no se repite
+	}
+    }
+    final_word_matrix.push_back(final_word_vector);                          //Guardamos el nuevo vector en la nueva matriz
+    final_word_vector.clear();                                               //Limpiamos el vector y volvemos a empezar desde el primer bucle con el segundo parrafo
+  }
+  for(int j=0;j<final_word_matrix[0].size();j++)
+    cout << final_word_matrix[0][j] << endl;
+  return final_word_matrix;
 }
 
  
@@ -144,7 +164,7 @@ int main(){
   a.load_text();
   //a.get_text();
   a.get_word_matrix();
-  cout << a.get_paragraph()[1] << endl;;
+  cout << a.get_paragraph()[0] << endl;;
   // for(int i=0;i<(a.get_word_list()).size();i++)
   //   cout << a.get_word_list()[i] <<endl;
   
